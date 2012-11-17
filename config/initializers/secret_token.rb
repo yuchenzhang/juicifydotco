61645fff493b14106f43de145a74940da8f98dbc
Yuchen Zhang <yuchenzhang@me.com>
2012-11-17 12:01:46 +0100

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Juicify::Application.config.secret_token = '95ba92a4178f11db4854934b0c31cd11149f5603f6213dfc8d31440517846929ee9c66412158914b8a222cab4bb87eec2da0fb73e754774acdb233dee6c9711b'
